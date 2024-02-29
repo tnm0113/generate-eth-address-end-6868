84a51d22c151e0c9f4c8846a4ca261a64687b422
Toan Nguyen <toanguyen@Toans-MacBook-Air.local>
2024-02-29 22:11:54 +0700

    push to git

---
+++ index.js
@@ -0,0 +1,8 @@
+import { generate } from "generate-nice-eth-address";
+
+function generateAddress6868(postfix) {
+    let result = generate("6868");
+    return result;
+}
+
+export { generateAddress6868 };
